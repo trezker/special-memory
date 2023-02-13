e4c3d1d4471781dfa5446df341b3905558f32023
Anders Andersson <trezker@gmail.com>
2023-02-13 18:34:51 +0100

    No duplicate table names

--- database/database.c
+++ database/database.c
@@ -14,7 +14,19 @@ void db_close(Database* db) {
 	free(db);
 }
 
+uint32_t db_find_table(Database* db, const char* name) {
+	for(uint32_t i = 0; i<db->num_tables; ++i) {
+		if(strncmp(name, db->tables[i].name, 64) == 0) {
+			return i;
+		}
+	}
+	return UINT32_MAX;
+}
+
 void db_create_table(Database* db, const char* name) {
+	if(db_find_table(db, name) != UINT32_MAX) {
+		return;
+	}
 	db->tables = realloc(db->tables, sizeof(Table)*(db->num_tables+1));
 	strncpy(db->tables[db->num_tables].name, name, 64);
 	db->num_tables++;
@@ -25,11 +37,9 @@ const char* db_first_table(Database* db) {
 }
 
 const char* db_next_table(Database* db, const char* name) {
-	
-	for(uint32_t i = 0; i<db->num_tables-1; ++i) {
-		if(strncmp(name, db->tables[i].name, 64) == 0) {
-			return db->tables[i+1].name;
-		}
+	uint32_t i = db_find_table(db, name);
+	if(i+1 < db->num_tables) {
+		return db->tables[i+1].name;
 	}
 	return NULL;
 }
