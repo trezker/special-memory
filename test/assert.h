1b2c9d801219c6c668fa2fa74dec260b7efbe574
Anders Andersson <trezker@gmail.com>
2023-02-26 17:04:26 +0100

    Improved assert prints

--- test/assert.h
+++ test/assert.h
@@ -14,21 +14,25 @@
 
 #define assert_equal_string(a, b) { \
 	if(strncmp(a, b, strlen(a)) != 0) { \
-		printf("%s:%i: Expected \"%s\", got \"%s\"\n", __FILE__, __LINE__, a, b); \
+		printf("%s:%i: \n\tExpected: \"%s\" \n\tActual:   \"%s\"\n", __FILE__, __LINE__, a, b); \
 		exit(EXIT_FAILURE); \
 	} \
 }
 
 #define assert_equal(a, b) { \
 	if(a != b) { \
-		printf("%s:%i: Expected \"%i\", got \"%i\"\n", __FILE__, __LINE__, a, b); \
+		printf("%s:%i: \n\tExpected: %i \n\tActual:   %i\n", __FILE__, __LINE__, a, b); \
 		exit(EXIT_FAILURE); \
 	} \
 }
 
 #define assert_equal_uuid(a, b) { \
 	if(uuid_compare(a, b) != 0) { \
-		printf("%s:%i: Expected \"%s\", got \"%s\"\n", __FILE__, __LINE__, a, b); \
+		char uta[36]; \
+		char utb[36]; \
+		uuid_unparse(a, uta); \
+		uuid_unparse(a, utb); \
+		printf("%s:%i: \n\tExpected: \"%s\" \n\tActual:   \"%s\"\n", __FILE__, __LINE__, uta, utb); \
 		exit(EXIT_FAILURE); \
 	} \
 }
